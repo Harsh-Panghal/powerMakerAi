659094aaf018815c29b3625d169e329a2c8156d6
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-08-31 18:47:22 +0530

    apply bg-layout-main to chatArea

--- src/components/PowerMakerLayout.tsx
+++ src/components/PowerMakerLayout.tsx
@@ -15,7 +15,7 @@ export function PowerMakerLayout() {
           <div className="flex-shrink-0">
             <PowerMakerHeader />
           </div>
-          <div className="flex-1 min-h-0">
+          <div className="flex-1 min-h-0 bg-layout-main">
             <ChatArea />
           </div>
         </div>
